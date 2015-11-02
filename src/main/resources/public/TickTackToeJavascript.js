89e40d8230316c8ea2f01497367850130f0557bf
saraarnad <saraarn13@ru.is>
2015-11-02 20:44:24 +0000

    checking if javascript works

--- src/main/resources/public/TickTackToeJavascript.js
+++ src/main/resources/public/TickTackToeJavascript.js
@@ -2,18 +2,6 @@ $( function() {
     var thisID = $(this).attr('id');
 
     $('td').click( function() {
-        if($(this).html()==""){
-            $.ajax({
-            type: "thisID",
-            url: "/thisID",
-            data:"thisID=" + thisID
-           
-           }).done(function(result){
-               $(this).html(result);
-        });
-        };
-        event.preventDefault();
-
-        $(this).toggleClass("red-cell");
+      $(this).toggleClass("red-cell");
     } );
 } );
