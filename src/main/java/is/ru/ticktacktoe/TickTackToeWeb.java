2ed609646d4f5c786a4e9fc5580309e3a36ca685
saraarnad <saraarn13@ru.is>
2015-11-02 16:11:26 +0000

    took out the star rule in checkstyle

--- src/main/java/is/ru/ticktacktoe/TickTackToeWeb.java
+++ src/main/java/is/ru/ticktacktoe/TickTackToeWeb.java
@@ -4,26 +4,23 @@ import spark.*;
 import static spark.Spark.*;
 import spark.servlet.SparkApplication;
 
-public class TickTackToeWeb implements SparkApplication
-{
+public class TickTackToeWeb implements SparkApplication{
 	private TickTackToe game;
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args){
 		staticFileLocation("/public");
-        SparkApplication TickTackToeWeb = new TickTackToeWeb();
+        SparkApplication tickTackToeWeb = new TickTackToeWeb();
 
         String port = System.getenv("PORT");
-        if (port != null) {
+        if (port != null){
             port(Integer.valueOf(port));
         }
 
-        TickTackToeWeb.init();
+        tickTackToeWeb.init();
 	}
 
 	@Override
-	public void init()
-	{
+	public void init(){
 		get("/hello", (req, res) -> "Hello World");
 	}
 }
